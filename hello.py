f2df3fe20e95bb0b2aa7827b1be123eb183f05b5
syoh5188 <syoh5188@gmail.com>
2022-05-10 21:22:27 +0900

    Create new Python file for test at 2022-05-10 21:22

---
+++ hello.py
@@ -0,0 +1,11 @@
+# This is test python file for team project git testing
+
+print('This team project is for EDA')
+
+for i in range(1, 5+1):
+	if i % 2 == 0:
+		print('Even Num')
+	else:
+		print('Odd Num')
+
+print('Running num is done')
